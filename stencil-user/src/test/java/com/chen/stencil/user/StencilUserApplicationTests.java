cb2facc159d37ed199ef5eb414e3ff74556426eb
chenweibo <563960993@qq.com>
2020-07-13 09:03:41 +0800

    Update StencilUserApplicationTests.java

--- stencil-user/src/test/java/com/chen/stencil/user/StencilUserApplicationTests.java
+++ stencil-user/src/test/java/com/chen/stencil/user/StencilUserApplicationTests.java
@@ -1,5 +1,6 @@
 package com.chen.stencil.user;
 
+import cn.hutool.extra.mail.MailUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,13 @@ class StencilUserApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    public void sentEmail() throws Exception {
+        MailUtil.send("563960993@qq.com", "测试", "邮件来自Hutool测试", false);
+
     }
 
 }
