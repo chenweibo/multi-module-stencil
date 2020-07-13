4b31b5d1d1b33b2603c53704cac8a6b85d571c9e
chenweibo <563960993@qq.com>
2020-07-13 14:14:53 +0800

    Sign in with

---
+++ stencil-user/src/main/java/com/chen/stencil/user/controller/RestAuthController.java
@@ -0,0 +1,313 @@
+package com.chen.stencil.user.controller;
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.chen.stencil.common.exception.Asserts;
+import com.xkcoding.http.config.HttpConfig;
+import io.swagger.annotations.Api;
+import me.zhyd.oauth.config.AuthConfig;
+import me.zhyd.oauth.model.AuthCallback;
+import me.zhyd.oauth.model.AuthResponse;
+import me.zhyd.oauth.model.AuthUser;
+import me.zhyd.oauth.request.*;
+import me.zhyd.oauth.utils.AuthStateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Api(tags = "第三方登录管理")
+@Controller
+@RequestMapping("/oauth")
+public class RestAuthController {
+
+
+    @RequestMapping("/render/{source}")
+    @ResponseBody
+    public void renderAuth(@PathVariable("source") String source, HttpServletResponse response) throws IOException {
+        System.out.println("进入render：" + source);
+        AuthRequest authRequest = getAuthRequest(source);
+        String authorizeUrl = authRequest.authorize(AuthStateUtils.createState());
+        System.out.println(authorizeUrl);
+        response.sendRedirect(authorizeUrl);
+    }
+
+    /**
+     * oauth平台中配置的授权回调地址，以本项目为例，在创建github授权应用时的回调地址应为：http://127.0.0.1:8443/oauth/callback/github
+     */
+    @RequestMapping("/callback/{source}")
+    public ModelAndView login(@PathVariable("source") String source, AuthCallback callback, HttpServletRequest request) {
+        System.out.println("进入callback：" + source + " callback params：" + JSONObject.toJSONString(callback));
+        AuthRequest authRequest = getAuthRequest(source);
+        AuthResponse<AuthUser> response = authRequest.login(callback);
+        System.out.println(JSONObject.toJSONString(response));
+
+//        if (response.ok()) {
+//            userService.save(response.getData());
+//            return new ModelAndView("redirect:/users");
+//        }
+//
+//        Map<String, Object> map = new HashMap<>(1);
+//        map.put("errorMsg", response.getMsg());
+
+        return new ModelAndView("error");
+    }
+
+    /**
+     * 根据具体的授权来源，获取授权请求工具类
+     *
+     * @param source
+     * @return
+     */
+    private AuthRequest getAuthRequest(String source) {
+        AuthRequest authRequest = null;
+        switch (source.toLowerCase()) {
+            case "dingtalk":
+                authRequest = new AuthDingTalkRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "baidu":
+                authRequest = new AuthBaiduRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "weibo":
+                authRequest = new AuthWeiboRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "coding":
+                authRequest = new AuthCodingRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .codingGroupName("")
+                        .build());
+                break;
+            case "oschina":
+                authRequest = new AuthOschinaRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "alipay":
+                // 支付宝在创建回调地址时，不允许使用localhost或者127.0.0.1，所以这儿的回调地址使用的局域网内的ip
+                authRequest = new AuthAlipayRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .alipayPublicKey("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "qq":
+                authRequest = new AuthQqRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "wechat_open":
+                authRequest = new AuthWeChatOpenRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "csdn":
+                authRequest = new AuthCsdnRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "taobao":
+                authRequest = new AuthTaobaoRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "google":
+                authRequest = new AuthGoogleRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        // 针对国外平台配置代理
+                        .httpConfig(HttpConfig.builder()
+                                .timeout(15000)
+                                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 10080)))
+                                .build())
+                        .build());
+                break;
+            case "facebook":
+                authRequest = new AuthFacebookRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        // 针对国外平台配置代理
+                        .httpConfig(HttpConfig.builder()
+                                .timeout(15000)
+                                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 10080)))
+                                .build())
+                        .build());
+                break;
+            case "douyin":
+                authRequest = new AuthDouyinRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "linkedin":
+                authRequest = new AuthLinkedinRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "microsoft":
+                authRequest = new AuthMicrosoftRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "mi":
+                authRequest = new AuthMiRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "toutiao":
+                authRequest = new AuthToutiaoRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "teambition":
+                authRequest = new AuthTeambitionRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "pinterest":
+                authRequest = new AuthPinterestRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "renren":
+                authRequest = new AuthRenrenRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "stack_overflow":
+                authRequest = new AuthStackOverflowRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .stackOverflowKey("")
+                        .build());
+                break;
+            case "huawei":
+                authRequest = new AuthHuaweiRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "wechat_enterprise":
+                authRequest = new AuthWeChatEnterpriseRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .agentId("")
+                        .build());
+                break;
+            case "kujiale":
+                authRequest = new AuthKujialeRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "gitlab":
+                authRequest = new AuthGitlabRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "meituan":
+                authRequest = new AuthMeituanRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "eleme":
+                authRequest = new AuthElemeRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "twitter":
+                authRequest = new AuthTwitterRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        // 针对国外平台配置代理
+                        .httpConfig(HttpConfig.builder()
+                                .timeout(15000)
+                                .proxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 10080)))
+                                .build())
+                        .build());
+                break;
+            case "wechat_mp":
+                authRequest = new AuthWeChatMpRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            case "aliyun":
+                authRequest = new AuthAliyunRequest(AuthConfig.builder()
+                        .clientId("")
+                        .clientSecret("")
+                        .redirectUri("")
+                        .build());
+                break;
+            default:
+                break;
+        }
+        if (null == authRequest) {
+            Asserts.fail("配置文件不存在");
+        }
+        return authRequest;
+    }
+}
